c4ac441fcce010062e36a4cbb3a1fbb06ae84d2b
neo-Gi <1904193389@qq.com>
2020-06-21 02:42:47 +0800

    修改Bean

--- src/main/java/cn/stb/stbcrmserver/domain/StaffRole.java
+++ src/main/java/cn/stb/stbcrmserver/domain/StaffRole.java
@@ -4,8 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.joda.time.DateTime;
 
-import java.util.Date;
 
 /**
  * 员工角色表的bean
@@ -15,11 +15,11 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 public class StaffRole {
-    private String StaffId;//员工ID
-    private String RoleId;//角色ID
-    private String RoleState;//角色状态：0无效、1有效
-    private String OperatorId;//操作人
-    private Date CreateTime;//创建时间
-    private Date UpdateTime;//更新时间
-    private String Remark;//备注
+    private String staffId;//员工ID
+    private String roleId;//角色ID
+    private String roleState;//角色状态：0无效、1有效
+    private String operatorId;//操作人
+    private DateTime createTime;//创建时间
+    private DateTime updateTime;//更新时间
+    private String remark;//备注
 }
