1ad5280b855a525935718bb6109e89d70b1f58b5
neo-Gi <1904193389@qq.com>
2020-06-21 00:07:28 +0800

    修改staff实例类

--- src/main/java/cn/stb/stbcrmserver/domain/Staff.java
+++ src/main/java/cn/stb/stbcrmserver/domain/Staff.java
@@ -18,7 +18,7 @@ public class Staff {
     private String StaffId;//员工ID
     private String StaffName;//员工姓名
     private String StaffCode;//员工工号,用于账号登录
-    private String StaffPassWd;//员工密码
+    private String PassWord;//员工密码
     private String StaffPhone;//员工电话
     private String StaffType;//员工类型:0系统管理员，1普通员工
     private String StaffState;//员工状态：1正常、9离职
