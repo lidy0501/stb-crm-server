8a8b62ae95835125f9d3e11e557816b40b1fe64b
neo-Gi <1904193389@qq.com>
2020-07-09 00:24:14 +0800

    修改order,添加UserVo

--- src/main/java/cn/stb/stbcrmserver/domain/Order.java
+++ src/main/java/cn/stb/stbcrmserver/domain/Order.java
@@ -17,11 +17,13 @@ import org.joda.time.DateTime;
 public class Order {
     private String orderId;//订单ID
     private String userId;//客户ID
+    private String userName;//客户姓名
+    private String company;//公司名称
     private String operatorId;//跟单人ID
     private String productName;//产品名称
     private String productSpe;//产品规格
     private int productNum;//产品数量
-    private String orderState;//订单状态0:未完成 1:已完成
+    private String orderState;//订单状态0:未完成 1:已完成 9:已删除
     private String payType;//支付方式
     private int totalFee;//订单应付总金额
     private int downPayFee;//首付款
@@ -30,7 +32,6 @@ public class Order {
     private String deliveryTime;//交期
     private String deliveryNo;//物流单号
     private String payRecord;//付款记录
-    private String orderDelete;//订单删除0:未删除 1:删除
     private DateTime createTime;//创建时间
     private DateTime updateTime;//更新时间
     private String remark;//备注
---
+++ src/main/java/cn/stb/stbcrmserver/vo/UserVo.java
@@ -0,0 +1,16 @@
+package cn.stb.stbcrmserver.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserVo {
+    private String userId;
+    private String userName;
+    private String company;
+}
